d8b659dc5df29cd48a5bfc1472f3a02e110fba06
Nagisa <2964793117@qq.com>
2025-01-26 00:22:12 +0800

    feat(Hardware/mpu6050.cpp):调试oled-mpu联调C++代码完成，移植工作结束
    可以在其中找到波特oled驱动的使用

--- Core/Src/maincpp.cpp
+++ Core/Src/maincpp.cpp
@@ -2,7 +2,7 @@
  * @Author: Nagisa 2964793117@qq.com
  * @Date: 2025-01-24 23:18:57
  * @LastEditors: Nagisa 2964793117@qq.com
- * @LastEditTime: 2025-01-25 11:38:10
+ * @LastEditTime: 2025-01-26 00:12:21
  * @FilePath: \MDK-ARMf:\project\git\MPU_OLED\Core\Src\maincpp.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,10 +10,13 @@
 #include "oled.h"
 #include "mpu6050.h"
 #include "font.h"
+#include <cstdio>
+
 
 //实例化
 OLED::OLED_t oled(&hi2c2);
 MPU::MPU6050_t mpu6050(&hi2c1);
+char buffer[20];
 
 void InitAll()
 {
@@ -23,13 +26,19 @@ void InitAll()
 void onceloop()
 {
     mpu6050.MPU6050_Read_All();
-    HAL_Delay (1);
+    HAL_Delay(1);
     oled.OLED_NewFrame();
-    oled.OLED_PrintASCIIString(0,0,"Pitch",&afont12x6,OLED_COLOR_NORMAL);
-    oled.OLED_PrintASCIIString(9,0,"roll",&afont12x6,OLED_COLOR_NORMAL);
-    oled.OLED_PrintASCIIString(18,0,"yaw",&afont12x6,OLED_COLOR_NORMAL);
-    oled.OLED_ShowFrame();
+    oled.OLED_PrintASCIIString(3,0,"Pitch:",&afont16x8,OLED_COLOR_NORMAL);
+    snprintf(buffer, sizeof(buffer), "%f", mpu6050._MPU6050.KalmanAngleX);
+    oled.OLED_PrintASCIIString(50,0,buffer,&afont16x8,OLED_COLOR_NORMAL);
 
+    oled.OLED_PrintASCIIString(3,20,"roll:",&afont16x8,OLED_COLOR_NORMAL);
+    snprintf(buffer, sizeof(buffer), "%f", mpu6050._MPU6050.KalmanAngleY);
+    oled.OLED_PrintASCIIString(50,20,buffer,&afont16x8,OLED_COLOR_NORMAL);
+
+    oled.OLED_PrintASCIIString(3,40,"yaw:",&afont16x8,OLED_COLOR_NORMAL);
+
+    oled.OLED_ShowFrame();
 }
 
 
--- Hardware/mpu6050.h
+++ Hardware/mpu6050.h
@@ -2,7 +2,7 @@
  * @Author: Nagisa 2964793117@qq.com
  * @Date: 2025-01-23 21:35:56
  * @LastEditors: Nagisa 2964793117@qq.com
- * @LastEditTime: 2025-01-25 00:06:01
+ * @LastEditTime: 2025-01-25 23:45:55
  * @FilePath: \MDK-ARMf:\project\git\MPU_OLED\Hardware\mpu6050.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -44,8 +44,8 @@ namespace MPU
 
         float Temperature;
 
-        double KalmanAngleX;
-        double KalmanAngleY;
+        double KalmanAngleX;//pitch
+        double KalmanAngleY;//roll
     } MPU6050;
 
     // Kalman structure
